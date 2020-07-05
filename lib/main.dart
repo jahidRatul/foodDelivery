282821e898a9255d483035ac5a12584364733e2c
jahidRatul <jahidratul@jahidRatuls-MacBook-Pro.local>
2020-07-05 13:11:04 +0600

    homepage ui v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'CookSpace',
 
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.deepPurple,
       ),
       home: MyHomePage(),
     );
@@ -80,128 +81,95 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class HomeItems extends StatelessWidget {
-  final Color color1 = Color(0xff511845);
-  final Color color2 = Color(0xff900c3f);
-  final Color color3 = Color(0xffc70039);
-  final Color color4 = Color(0xffff5733);
+
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-
-      Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [color3, color4],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter)),
-      ),
-      Container(
-        height: 490,
-        width: 300,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(bottomRight: Radius.circular(30)),
-            color: color2),
-      ),
-      Container(
-        height: 370,
-        width: 170,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(bottomRight: Radius.circular(30)),
-            color: color1),
-      ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
 
 
-        SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
+          SizedBox(height: 10.0),
+          Container(
+            child: Column(
+              children: <Widget>[
 
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    child: TextField(
+                      // controller: TextEditingController(text: locations[0]),
+                      cursorColor: Theme.of(context).primaryColor,
 
-              SizedBox(height: 10.0),
-              Container(
-                child: Column(
-                  children: <Widget>[
-
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                        child: TextField(
-                          // controller: TextEditingController(text: locations[0]),
-                          cursorColor: Theme.of(context).primaryColor,
-
-                          decoration: InputDecoration(
-                              hintText: "Search Food ",
-                              hintStyle: TextStyle(
-                                  color: Colors.black38, fontSize: 16),
-                              prefixIcon: Material(
-                                elevation: 0.0,
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(30)),
-                                child: Icon(Icons.search),
-                              ),
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 25, vertical: 13)),
-                        ),
-                      ),
+                      decoration: InputDecoration(
+                          hintText: "Search Food ",
+                          hintStyle: TextStyle(
+                              color: Colors.black38, fontSize: 16),
+                          prefixIcon: Material(
+                            elevation: 0.0,
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(30)),
+                            child: Icon(Icons.fastfood),
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 13)),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Text(
-                  "Popular Food Items",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                height: 250,
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 8,
-                  itemBuilder: _buildItem1,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Text(
-                  "Regular Food Items",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                height: 100,
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 8,
-                  itemBuilder: _buildItem2,
-                ),
-              ),
-
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+          SizedBox(
+            height: 10,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Text(
+              "Popular Food Items",
+              style: GoogleFonts.lobster(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0  ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 250,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 16.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: 8,
+              itemBuilder: _buildItem1,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Text(
+              "Regular Food Items",
+               style: GoogleFonts.lobster(
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0  ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 16.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: 8,
+              itemBuilder: _buildItem2,
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 
@@ -231,7 +199,7 @@ class HomeItems extends StatelessWidget {
             borderRadius: BorderRadius.circular(16),
           ),
           color: Colors.transparent,
-          elevation: 1,
+          elevation: 2,
           child: Container(
 //            color: Color(0xff00005c),
             width: 140,
@@ -251,17 +219,15 @@ class HomeItems extends StatelessWidget {
                 Text(
 
                   'Fast Food Items',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                      color: Colors.white
-
+                  style: GoogleFonts.questrial(
+                    fontSize: 16
                   ),
                 ),
                 Text(
                   'TK. 180',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                      color: Colors.tealAccent
+                      color: Colors.purple
 
                   ),
                 ),
@@ -269,10 +235,10 @@ class HomeItems extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     IconButton(
-                        icon: Icon(Icons.favorite_border, color: Colors.amberAccent),
+                        icon: Icon(Icons.favorite_border, color: Colors.red),
                         onPressed: (){}),
                     IconButton(
-                        icon: Icon(Icons.add_shopping_cart, color: Colors.cyan),
+                        icon: Icon(Icons.add_shopping_cart, color: Colors.cyanAccent),
                         onPressed: (){}),
                   ],
                 )
@@ -298,7 +264,7 @@ class HomeItems extends StatelessWidget {
             borderRadius: BorderRadius.circular(16),
           ),
           color: Colors.transparent,
-          elevation: 1,
+          elevation: 2,
           child: Container(
 
 
@@ -326,29 +292,29 @@ class HomeItems extends StatelessWidget {
                     height: 90,
                     child: Column(
                       children: <Widget>[
+
                         Text(
 
                           'Fast Food Items',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white
+                          style: GoogleFonts.questrial(
+                            fontSize: 16
                           ),
                         ),
                         Text(
                           'TK. 180',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
-                              color: Colors.tealAccent
+                              color: Colors.purple
                           ),
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             IconButton(
-                                icon: Icon(Icons.favorite_border, color: Colors.amberAccent),
+                                icon: Icon(Icons.favorite_border, color: Colors.red),
                                 onPressed: (){}),
                             IconButton(
-                                icon: Icon(Icons.add_shopping_cart, color: Colors.cyan),
+                                icon: Icon(Icons.add_shopping_cart, color: Colors.cyanAccent),
                                 onPressed: (){}),
                           ],
                         )
